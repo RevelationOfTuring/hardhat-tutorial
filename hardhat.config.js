09949bbd0b44ded8340371ed41499f7142c2a7c3
MichaelWang <michael_wangguanyi@163.com>
2021-04-22 17:02:06 +0800

    upd

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,22 @@ require("@nomiclabs/hardhat-waffle")
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ExchainUrl = "http://18.167.142.95:26659";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
 module.exports = {
     solidity: "0.7.3",
+    networks: {
+        exchain: {
+            url: ExchainUrl,
+            accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+        }
+    }
 };
